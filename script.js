fea427e97b7da5292cba4f368fda7b0e41ea8a38
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-03 23:38:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let displayTime = document.getElementById("timer");
+let currentTime = new Date();
+setInterval(
+	function(){
+		currentTime = new Date();
+		displayTime.innerHTML = currentTime.toLocaleString();
+	}, 1000
+);
